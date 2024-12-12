c0ddad52d39cc08f7621d19a103d0f650afa13d7
Paperclip <131926845+itsBuggingMe@users.noreply.github.com>
2024-12-12 18:10:26 +0000

    day12

--- AdventOfCode2024/Solutions/Day12.cs
+++ AdventOfCode2024/Solutions/Day12.cs
@@ -9,9 +9,9 @@ namespace AdventOfCode2024.Solutions;
 internal class Day12 : ISolution
 {
     public int Day => 12;
-    public object? Solve1(string input) => 
+    public object? Solve1(string input) =>
         input.IndexOf('\n') is int inputWidth &&
-        new int[] { 1, -1, -inputWidth - 1, inputWidth + 1 } is { } deltas && 
+        new int[] { 1, -1, -inputWidth - 1, inputWidth + 1 } is { } deltas &&
         new HashSet<int>() is { } large ?
             input
             .Select((c, i) => (i, c))
@@ -26,7 +26,7 @@ internal class Day12 : ISolution
                 set : deltas
                     .Select(i => func(self, str, width, x + i, tile, deltas, set, total))
                     .Where(t => true)
-                    .Last()) is Func<object, string, int, int, char, int[], HashSet<int>, HashSet<int>, HashSet<int>> func ? 
+                    .Last()) is Func<object, string, int, int, char, int[], HashSet<int>, HashSet<int>, HashSet<int>> func ?
                     (set: func(func, input, inputWidth, c.i, c.c, deltas, new(), large), c.c) : throw null!)
             .Where(t => t.set.Count != 0)
             .Sum(t => t.set.Count * t.set.Sum(p => deltas.Where(d => p + d is int x && (x < 0 || x >= input.Length || input[x] != t.c)).Count()))
@@ -52,13 +52,12 @@ internal class Day12 : ISolution
                     .Last()) is Func<object, string, int, int, char, int[], HashSet<int>, HashSet<int>, HashSet<int>> func ?
                     (set: func(func, input, inputWidth, c.i, c.c, deltas, new(), large), c.c) : throw null!)
             .Where(t => t.set.Count != 0)
-            .Sum(t => CountSides(t.set, inputWidth + 1, deltas, input, t.c) * t.set.Count)
+            .Sum(t => t.set.SelectMany(p => deltas.Where(d => p + d is int x && (x < 0 || x >= input.Length || input[x] != t.c))
+            .Select(d => (normal: d, pos: p)))
+            .GroupBy(a => Math.Abs(a.normal) == 1 ? (a.normal, a.pos % (inputWidth + 1)) : (a.normal, a.pos / (inputWidth + 1)))
+                .Select(a => a.OrderBy(c => c.pos)).Sum(a => a.Skip(1)
+                    .Aggregate(
+                        (sum: 1, prev: a.First().pos, expected: Math.Abs(a.First().normal) == 1 ? (inputWidth + 1) : 1),
+                        (a, c) => (sum: a.sum + Convert.ToInt32(a.prev + a.expected != c.pos), prev: c.pos, a.expected)).sum) * t.set.Count)
         : throw null!;
-
-    private static int CountSides(HashSet<int> sides, int width, int[] deltas, string input, char c)
-    {
-        var x = sides.SelectMany(p => deltas.Where(d => p + d is int x && (x < 0 || x >= input.Length || input[x] != c)).Select(d => (normal: d, pos: p))).ToArray();
-        var y = x.GroupBy(t => Math.Abs(t.normal) == 1 ? (t.normal, edgepos: t.pos % width) : (t.normal, edgepos: t.pos / width)).ToArray();
-        return y.Length;
-    }
 }
