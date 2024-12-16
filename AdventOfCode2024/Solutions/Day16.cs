72a566fcb0afb3239ac55a97d1fa0e6c40f50f40
Paperclip <131926845+itsBuggingMe@users.noreply.github.com>
2024-12-16 11:11:42 -0500

    initial work on dijkstra's

--- AdventOfCode2024/Solutions/Day16.cs
+++ AdventOfCode2024/Solutions/Day16.cs
@@ -10,7 +10,7 @@ internal class Day16 : ISolution
         input.IndexOf('\n') + 1  is { } width &&
         input.IndexOf('S')  is { } start &&
         input.IndexOf('E')  is { } end &&
-        new (int Forward, int TurnA, int TurnB)[]
+        new (int F, int A, int B)[]
         {
             (-width, 1, -1),
             (1, width, -width),
@@ -19,9 +19,14 @@ internal class Day16 : ISolution
         } is { } deltas &&
         new HashSet<int>() is { } visited &&
         new SortedDictionary<int, int>(input.Select((c, i) => (i, c)).Where(c => c.c == '.').ToDictionary(c => c.i, c => int.MaxValue))
-         is { } buff ?
+         is { } dists ?
             Enumerable.Range(0, int.MaxValue)
-            .Select(t => new int[] {  })
+            .Select(t => dists[start + deltas[direction].F] = 1)
+            .Select(t => dists[start + deltas[direction].A] = 1000)
+            .Select(t => dists[start + deltas[direction].B] = 1000)
+            .Select(t => visited.Add(start))
+            .Select(t => start =  dists.MinBy(kvp => kvp.Value).Key)
+            .TakeWhile(t => input[start] != 'E')
         : throw null!;
 
     public object? Solve2(string input) => input;
